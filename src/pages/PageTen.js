9463d2d97d87e680b37890710c2eed4c984b9224
joegelay <joegelay@gmail.com>
2021-05-01 21:58:18 -0600

    finish end page CTA content

--- src/pages/PageTen.js
+++ src/pages/PageTen.js
@@ -3,22 +3,27 @@ export default function PageTen() {
     <div className="page page-ten globe-background">
       <div className="row flex-row">
         <div className="column flex-column">
-          <h3>
-            According to the World Bank, recent estimates for global poverty are
-            that at least{" "}
-            <strong className="black">10%&nbsp;of&nbsp;the&nbsp;world</strong>,
-            or over{" "}
-            <strong className="black">750&nbsp;million&nbsp;people</strong>,
-            live in extreme poverty on <strong>$1.90</strong> or less a day.
-          </h3>
-          <h5>
-            * This figure is adjusted for factors such as exchange rate,
-            purchasing power, or farmers that can subside partially off of their
-            own food production.
-          </h5>
+          <h1>
+            While you might not be Billionaire Bezos, perhaps you're realizing
+            how relatively rich you are to so many others.
+          </h1>
+          <h2>
+            Want to learn more about how you can help improve the lives of
+            millions?
+          </h2>
+          <h2>
+            Visit{" "}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.givewell.org/"
+            >
+              GiveWell.org
+            </a>{" "}
+            to donate to the planet's most effective charities.
+          </h2>
         </div>
       </div>
-      <div className="row flex-row"></div>
     </div>
   );
 }
